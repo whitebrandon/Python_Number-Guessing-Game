e2afbb6d3c9aea1213c0dd9ad9ad04399fd4cc13
Brandon White <white.brandonsean@gmail.com>
2020-07-17 21:13:41 -0400

    updated application.py

--- application.py
+++ application.py
@@ -1,30 +1,49 @@
 import random
+import time
 import sys
+
+# Constants
+MIN_NUM = 1
+MAX_NUM = 10
 
 ## sys.exit("Too many invalid password attempts) ← To exit loop
 
-
+intro = ["YO, I'M PY THE RAPPER, HERE TO EXPLAIN...", "HOW TO PLAY THIS RANDOM NUMBER GUESSING GAME...", "HOPEFULLY IT'S FUN AND IT TESTS YOUR BRAIN...", "BUT BEFORE WE GO ON CAN I KNOW YOUR NAME?"]
 def start_game():
-	player_name = input("Welcome to the Random Number Guessing game. Please enter your name. ");
-	player_guess = ""
-	solution = random.randint(0, 10)
-	print("Hi, {}".format(player_name))
+	# for line in intro:
+	# 	print(line + " "*15, end="\r")
+	# 	time.sleep(2.5)
+	player_name = input("Let's get started. Please enter your name to continue. ")
+	player_guess = None
+	solution = random.randint(MIN_NUM, MAX_NUM)
+	print("Hi, {}. Let's begin.".format(player_name))
 	guess_count = 1
 	while player_guess != solution:
-		try:
-			while player_guess is not int:
-				try:
-					player_guess = int(input("Please select a number between 1 and 10. "))
-				except ValueError:
-					print("Oh no! That's not a valid value. Try again...")
+		if not isinstance(player_guess, int):
+			try:
+				player_guess = int(input("First select a number between 1 and 10.\n"))
+				if player_guess < MIN_NUM or player_guess > MAX_NUM:
+					raise ValueError("Don't forget, it's MORE than {}, but LESS than {}".format(MIN_NUM - 1, MAX_NUM + 1))
+			except ValueError as error:
+				print("Oops, that value won't work. Please try it again...")
+				if isinstance(player_guess, int):
+					print("({})".format(error))
+				continue
+		else:
+			try:	
 				if player_guess > solution:
-					player_guess = int(input("It's lower. "))
+					player_guess = int(input("It's lower.\n"))
 					guess_count += 1
 				elif player_guess < solution:
-					player_guess = int(input("It's higher. "))
+					player_guess = int(input("It's higher.\n"))
 					guess_count += 1
-		except ValueError:
-			print("Oh no! That's not a valid value. Try again...")
+			except ValueError:
+				if not isinstance(player_guess, int):
+					continue
+				else:
+					print("Oh no! That's not a valid value. Try again...")
+			except TypeError:
+				print("Guess should be a whole number. Please try again...")
 			
 	print("You got it right! Congrats! It took you {} guesses to find the number".format(guess_count))
 
